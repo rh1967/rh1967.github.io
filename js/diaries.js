6975cc5f0cdd5de87cb10c5abcfaedb72313aa8d
rh1967 <dev-ferdinand-I-app@github.io>
2023-02-22 11:02:00 +0000

    Update from https://github.com/rh1967/dev-zinzendorf-app/commit/85b09b43498e1f39052a60e3c7631e86043fa7e0

--- js/diaries.js
+++ js/diaries.js
@@ -4,52 +4,24 @@
         $("ul.navbar-nav li.nav-item a").removeClass("active");
         $( "[href='diaries.html']" ).addClass("active");
         
-        $("#indexLetters div.item").hide();
-        
-        let anchor = $(location).attr('hash');  //get link anchor (#...)                        
-        if(anchor.length!=0){  //check if link anchor exists
-        //$(anchor).click(); //click on the menu item to open element
-            var itemId = "#indexLetters " + anchor;
-            $( itemId ).show();
-        }                        
-        
-        browserStorage() ;                                                
-    }) ;
+        browserStorage() ;
+    });
+    
+    $( "ul.submenu-start li.list-item a" ).click(function() {
+        var click = $( this );
+        var href =  click.attr("href");        
+        var itemId = "ul.submenu-start li.list-item a[href='" + href + "']";
 
-    $( "div.langmenu a" ).click(function() {
-        var click_lang = $( this );
-        console.log( click_lang.attr( "lang" ));
-        langDeEn(click_lang.attr( "lang" )) ;                        
-    }) ;
-    
-    $( "table.metadata a" ).click(function() {
-        console.log( this );
-        var click = $( this );
-        var itemId = "#indexLetters " + click.attr("href");
-        console.log( itemId );
-        
-        $("#indexLetters div.item").hide();
-        $( itemId ).toggle();                        
-    })                    
-    
-    $( "div.letter a" ).click(function() {
-        console.log( this );
-        var click = $( this );
-        var itemId = "#indexLetters " + click.attr("href");
-        console.log( itemId );
-        
-        $("#indexLetters div.item").hide();
-        $( itemId ).toggle();                        
-    })
-    
-    $( "div.commentary a" ).click(function() {
-        console.log( this );
-        var click = $( this );
-        var itemId = "#indexLetters " + click.attr("href");
-        console.log( itemId );
-        
-        $("#indexLetters div.item").hide();
-        $( itemId ).toggle();
-    })
-                    
-                        
+        if (!click.hasClass("active") && !href.includes("_text")) {
+            $("ul.submenu-start li.list-item a").removeClass("active");
+            $("ul.submenu-start li.list-item div").removeClass("show");
+            $( itemId ).addClass("active");    
+        } else {
+            $( itemId ).removeClass("active");
+        }        
+    });
+
+    $( "ul.submenu-start li li.list-item a" ).click(function() {
+        var click = $( this );        
+        embedLoad( click ) ;
+    }); 
