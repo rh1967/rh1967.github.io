725606e179b983f9782761b748c53be4d7a35f5e
rh1967 <dev-ferdinand-I-app@github.io>
2023-06-15 16:36:10 +0000

    Update from https://github.com/rh1967/dev-ferdinand-I-app/commit/d009edb75ad4dfe8bc79563f8d91390782ba1c05

--- js/geoname_api.js
+++ js/geoname_api.js
@@ -10,8 +10,8 @@ function geoname_api() {
     }
     //convert html encoding to special characters
     let searchItem_char = convertHtml2Char(searchItem) ;
-
-    let url = "https://secure.geonames.org/searchJSON?q=" + searchItem_char + "&maxRows=10&username=" + OWNER ;
+    let maxRows = 50 ;
+    let url = "https://secure.geonames.org/searchJSON?q=" + searchItem_char + "&" + maxRows + "=10&username=" + OWNER ;
     var settings = {
         "url": url,
         //"url": "http://api.geonames.org/searchJSON?q=Adriatic Sea&maxRows=10&username=rh1967",
